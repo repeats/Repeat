9373a706d2e4c987298dd1f28e391f0d602c8fb4
hptruong93 <hptruong93@gmail.com>
2018-03-14 00:39:42 -0400

    Improve API for Pair class.

--- src/utilities/Pair.java
+++ src/utilities/Pair.java
@@ -5,6 +5,10 @@ public class Pair<A, B> {
 	private final A a;
 	private final B b;
 
+	public static<A,B> Pair<A, B> of(A a, B b) {
+		return new Pair<A, B>(a, b);
+	}
+
 	public Pair(A a, B b) {
 		this.a = a;
 		this.b = b;
