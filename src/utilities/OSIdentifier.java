fca231fa545396a316a9e4adb77e3a6be757e60b
hptruong93 <hptruong93@gmail.com>
2018-12-07 23:49:53 -0500

    Improve OS identifier utility.

--- src/utilities/OSIdentifier.java
+++ src/utilities/OSIdentifier.java
@@ -3,12 +3,16 @@ package utilities;
 public class OSIdentifier {
 
 	public static final String OS_NAME;
-	public static final boolean IS_WINDOWS, IS_UNIX;
+	public static final boolean IS_WINDOWS, IS_UNIX, IS_LINUX, IS_OSX, IS_UNKNOWN;
 
 	static {
-		OS_NAME = System.getProperty("os.name");
-		IS_WINDOWS = OS_NAME.startsWith("Windows");
+		// See http://lopica.sourceforge.net/os.html
+		OS_NAME = System.getProperty("os.name").toLowerCase();
+		IS_WINDOWS = OS_NAME.startsWith("win");
 		IS_UNIX = !IS_WINDOWS;
+		IS_OSX = OS_NAME.startsWith("mac");
+		IS_LINUX = OS_NAME.startsWith("linux");
+		IS_UNKNOWN = !IS_WINDOWS && !IS_LINUX && !IS_OSX;
 	}
 
 	private OSIdentifier() {}
