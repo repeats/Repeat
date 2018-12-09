7adc6ceac57eb1b738914c4d880d2e88858a57f5
hptruong93 <hptruong93@gmail.com>
2018-12-09 12:06:34 -0500

    Add native hook code and binaries.

---
+++ src/nativehooks/windows/nativecontent/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace RepeatHook
+{
+    class Program
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WH_MOUSE_LL = 14;
+        private static IntPtr _keyboardHookID = IntPtr.Zero;
+        private static IntPtr _mouseHookID = IntPtr.Zero;
+        private static LowLevelEventProc keyboardProc = KeyboardHookCallback;
+        private static LowLevelEventProc mouseProc = MouseHookCallback;
+
+        public static void Main()
+        {
+            _keyboardHookID = SetHook(keyboardProc, WH_KEYBOARD_LL);
+            _mouseHookID = SetHook(mouseProc, WH_MOUSE_LL);
+            Application.Run();
+            UnhookWindowsHookEx(_keyboardHookID);
+            UnhookWindowsHookEx(_mouseHookID);
+        }
+
+        private static IntPtr SetHook(LowLevelEventProc proc, int idHook)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(idHook, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        // Check SetWindowsHookEx documentation to see that both
+        // mouse and keyboard callback have the same signature.
+        private delegate IntPtr LowLevelEventProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr KeyboardHookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                int keyCode = Marshal.ReadInt32(lParam);
+                Console.WriteLine("K:" + keyCode + ",P:" + wParam);
+            }
+
+            return CallNextHookEx(_keyboardHookID, nCode, wParam, lParam);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MSLLHOOKSTRUCT
+        {
+            public POINT pt;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public UIntPtr dwExtraInfo;
+        }
+
+        private static IntPtr MouseHookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                MSLLHOOKSTRUCT mouseLowLevelHook = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                int x = mouseLowLevelHook.pt.x;
+                int y = mouseLowLevelHook.pt.y;
+                Console.WriteLine("M:" + x + "," + y + ",P:" + wParam);
+            }
+
+            return CallNextHookEx(_mouseHookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelEventProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
