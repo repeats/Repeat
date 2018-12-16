8753f9738de786565915049e35d87cf9071bb99b
hptruong93 <hptruong93@gmail.com>
2018-12-16 11:12:08 -0500

    Formatting

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		if (args.length == 0) {
 			MainFrontEnd.run();
-		} else if (args[0].equals("cli")){
+		} else if (args[0].equals("cli")) {
 			MainCli cli = new MainCli();
 			String[] cliArgs = Arrays.copyOfRange(args, 1, args.length);
 			cli.process(cliArgs);
