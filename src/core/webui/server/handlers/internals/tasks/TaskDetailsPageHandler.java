8458484e6709c9ab9ab17355547d52327ffccbfe
hptruong93 <hptruong93@gmail.com>
2022-11-13 19:09:34 -0500

    Use new constant strings.

--- src/core/webui/server/handlers/internals/tasks/TaskDetailsPageHandler.java
+++ src/core/webui/server/handlers/internals/tasks/TaskDetailsPageHandler.java
@@ -72,19 +72,19 @@ public class TaskDetailsPageHandler extends AbstractUIHttpHandler {
 
 	private Void handleNewHotkey(HttpAsyncExchange exchange, String taskString) throws IOException {
 		String activationConstructorId = "";
-		if (taskString.equals("record")) {
+		if (taskString.equals(RECORD_TASK_NAME)) {
 			KeyChain recordKeyChain = backEndHolder.getConfig().getRECORD();
 			activationConstructorId = taskActivationConstructorManager.addNewConstructor(TaskActivation.newBuilder().withHotKey(recordKeyChain).build(), TaskActivationConstructor.Config.ofRestricted().setDisableKeyChain(false));
 		}
-		if (taskString.equals("replay")) {
+		if (taskString.equals(REPLAY_TASK_NAME)) {
 			KeyChain replayKeyChain = backEndHolder.getConfig().getREPLAY();
 			activationConstructorId = taskActivationConstructorManager.addNewConstructor(TaskActivation.newBuilder().withHotKey(replayKeyChain).build(), TaskActivationConstructor.Config.ofRestricted().setDisableKeyChain(false));
 		}
-		if (taskString.equals("runCompiled")) {
+		if (taskString.equals(RUN_COMPILED_TASK_NAME)) {
 			KeyChain runCompiledKeyChain = backEndHolder.getConfig().getCOMPILED_REPLAY();
 			activationConstructorId = taskActivationConstructorManager.addNewConstructor(TaskActivation.newBuilder().withHotKey(runCompiledKeyChain).build(), TaskActivationConstructor.Config.ofRestricted().setDisableKeyChain(false));
 		}
-		if (taskString.equals("mouseGestureActivation")) {
+		if (taskString.equals(MOUSE_GESTURE_ACTIVATION_TASK_NAME)) {
 			KeyChain mouseGestureKeyChain = new KeyChain(backEndHolder.getConfig().getMouseGestureActivationKey());
 			activationConstructorId = taskActivationConstructorManager.addNewConstructor(TaskActivation.newBuilder().withHotKey(mouseGestureKeyChain).build(), TaskActivationConstructor.Config.ofRestricted().setDisableKeyChain(false).setMaxStrokes(1));
 		}
