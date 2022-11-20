ed7102e58d5d17ea48b66f731e39d1b554daaab1
hptruong93 <hptruong93@gmail.com>
2022-11-19 21:47:19 -0500

    ignore another known server exception

--- src/core/webui/server/UIServerExceptionLogger.java
+++ src/core/webui/server/UIServerExceptionLogger.java
@@ -14,6 +14,9 @@ public class UIServerExceptionLogger implements ExceptionLogger {
 			if (arg0.getLocalizedMessage().contains("An established connection was aborted by the software in your host machine")) {
 				return;
 			}
+			if (arg0.getLocalizedMessage().contains("An existing connection was forcibly closed by the remote host")) {
+				return;
+			}
 		}
 
 		arg0.printStackTrace();
