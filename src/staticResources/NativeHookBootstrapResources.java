230a68882520e52b0a8960a64eb75da07f774357
hptruong93 <hptruong93@gmail.com>
2022-11-23 22:37:04 -0500

    Update simple native hook dependency.

--- src/staticResources/NativeHookBootstrapResources.java
+++ src/staticResources/NativeHookBootstrapResources.java
@@ -1,13 +1,14 @@
 package staticResources;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 import org.simplenativehooks.staticResources.BootStrapResources;
 
 public class NativeHookBootstrapResources implements BootstrapResourcesExtrator {
 
 	@Override
-	public void extractResources() throws IOException {
+	public void extractResources() throws IOException, URISyntaxException {
 		BootStrapResources.extractResources();
 	}
 }
